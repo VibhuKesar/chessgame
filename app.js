008bdd2c62492bb0dcb276ca1f55d39a8ab91bc8
VibhuKesar <kesarvibhu1997@gmail.com>
2016-07-22 18:30:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.static('public')); 
 var http = require('http').Server(app);
-var io = require('socket.io').listen(8080);
-var count = 0
 
-io.sockets.on('connection', function(socket) {
-    count++;
-    io.sockets.emit('message', { count: count });
-
-    socket.on('disconnect', function(){
-        count--;
-        io.sockets.emit('message', { count: count });
-    })
-});
-
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
